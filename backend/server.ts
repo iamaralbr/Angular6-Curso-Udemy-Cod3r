968bfd652e7fb2851a4481e79b2611b280294920
Isac Amaral <isac.redua@gmail.com>
2020-07-28 21:21:08 -0300

    Segurança Backend

---
+++ backend/server.ts
@@ -0,0 +1,36 @@
+import * as jsonServer from 'json-server'
+import { Application } from 'express'
+
+import * as fs from 'fs'
+import * as https from 'https'
+
+import { handleAuthentication } from './auth'
+import { handleAuthorization } from './authz'
+
+const server: Application = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults() // defaults é um array de middlewares
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser) // necessário porque por padrão o express não faz o parse do body
+
+// 
+server.post('/login', handleAuthentication)
+server.use('/orders', handleAuthorization)
+
+// Use default router
+server.use(router)
+
+const options = {
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+const port: number = 3002
+https.createServer(options, server).listen(port, () => {
+  console.log(`JSON Server is running on https://localhost:${port}`)
+})
