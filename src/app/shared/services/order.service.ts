736461ec9245729739226dbe229483a0ff8cb009
Isac Amaral <isac.redua@gmail.com>
2020-07-30 17:35:56 -0300

    Segurança Frontend com Interceptors

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -8,14 +8,12 @@ import { CartItem } from '../../restaurants/restaurant/restaurant-detail/menu/sh
 import { Order } from '../../order/order.model';
 
 import { MEAT_API } from '../../app.api'
-import { LoginService } from './login.service'
 
 @Injectable()
 export class OrderService {
 
   constructor(private cartService: ShoppingCartService,
-    private http: HttpClient,
-    private loginService: LoginService) { }
+    private http: HttpClient) { }
 
   itemsValue(): number {
     return this.cartService.total()
@@ -42,10 +40,6 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<Order> {
-    let headers = new HttpHeaders()
-    if (this.loginService.isLoggedIn()) {
-      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
-    }
-    return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers })
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
   }
 }
